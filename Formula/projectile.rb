c09c86523ceb555bcc34cba3b6c146cb602beeed
Kyle Unverferth <kyle@kyleu.com>
2019-05-14 15:38:50 -0600

    Projectile homebrew recipe, first pass

---
+++ Formula/projectile.rb
@@ -0,0 +1,19 @@
+class Projectile < Formula
+  desc "Creates beautiful Scala code from your Postgres database, GraphQL queries, or Thrift definitions"
+  homepage "https://kyleu.com/projectile"
+  url "https://github.com/KyleU/projectile/releases/download/v1.8.6/projectile.jar"
+  sha256 "574c661b30b1202f1ee6e9780015f46f80ad82f79de11ed26aafccf9954be70f"
+
+  bottle :unneeded
+
+  depends_on :java => "1.8+"
+
+  def install
+    libexec.install "projectile-#{version}.jar"
+    bin.write_jar_script libexec/"projectile-#{version}.jar", "projectile"
+  end
+
+  test do
+    system "#{bin}/projectile"
+  end
+end

4bf5c2219911a8f4b3ec2fd543e73c8b61db3806
Kyle Unverferth <kyle@kyleu.com>
2019-05-14 20:34:47 -0600

    Database Flow homebrew recipe

---
+++ Formula/databaseflow.rb
@@ -0,0 +1,19 @@
+class DatabaseFlow < Formula
+  desc ""
+  homepage "https://databaseflow.com"
+  url "https://github.com/KyleU/databaseflow/releases/download/v1.7.0/databaseflow.jar"
+  sha256 "682c19888e29773669c368494704fdc74bd116567aad68fd9f6ccc996b737a69"
+
+  bottle :unneeded
+
+  depends_on :java => "1.8+"
+
+  def install
+    libexec.install "databaseflow.jar"
+    bin.write_jar_script libexec/"databaseflow.jar", "databaseflow"
+  end
+
+  test do
+    system "#{bin}/databaseflow"
+  end
+end

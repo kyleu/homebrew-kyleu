db0b599dedcfbb17fde2173c20012ce24b2df03c
Kyle Unverferth <kyle@kyleu.com>
2019-05-14 20:36:29 -0600

    Database Flow homebrew recipe

--- Formula/databaseflow.rb
+++ Formula/databaseflow.rb
@@ -1,5 +1,5 @@
-class DatabaseFlow < Formula
-  desc ""
+class Databaseflow < Formula
+  desc "An opinionated modern database interface"
   homepage "https://databaseflow.com"
   url "https://github.com/KyleU/databaseflow/releases/download/v1.7.0/databaseflow.jar"
   sha256 "682c19888e29773669c368494704fdc74bd116567aad68fd9f6ccc996b737a69"

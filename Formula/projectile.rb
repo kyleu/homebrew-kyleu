a2b0a80ec5f604d823635a7690f7291aa9ddf463
Kyle Unverferth <kyle@kyleu.com>
2019-08-19 13:19:21 -0600

    Projectile 1.13.4

--- Formula/projectile.rb
+++ Formula/projectile.rb
@@ -1,8 +1,8 @@
 class Projectile < Formula
   desc "Creates beautiful Scala code from your Postgres database, GraphQL queries, or Thrift definitions"
   homepage "https://kyleu.com/projectile"
-  url "https://github.com/KyleU/projectile/releases/download/v1.10.4/projectile.jar"
-  sha256 "52a940f21e13ddce6ce89062268fa351d2c7742c299dc99263102125792bd5c7"
+  url "https://github.com/KyleU/projectile/releases/download/v1.13.4/projectile.jar"
+  sha256 "06fb4fb04b7344bfc0e99de68dd89db1f9915363933872b1355e917c9b55305d"
 
   bottle :unneeded
 
